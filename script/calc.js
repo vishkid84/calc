f3a372b718f9967821797a199c05d12b0447ce26
vishkid84 <vishalittr@gmail.com>
2021-07-20 18:06:40 +0000

    appended number to display

--- script/calc.js
+++ script/calc.js
@@ -2,9 +2,18 @@ $('.calc_keys').click(function(e) {
     if ($( e.target ).is(":button")) {
         const key = e.target;
         const action = key.dataset.action;
+        const display = document.getElementById('calcDisplay');
+        const displayedNum = display.textContent;
+        // if no dataset, it would be a number
         if(!action){
-            console.log('number key pressed');
+            console.log(key.textContent);
+            if(displayedNum == 0){
+                display.textContent = key.textContent;
+            }else{
+                display.textContent = displayedNum + key.textContent;
+            }
         }
+        // if there is a dataset
         if(action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide'){
             console.log('calculation keys pressed');
         }
@@ -20,6 +29,7 @@ $('.calc_keys').click(function(e) {
         if(action === 'decimal'){
             console.log('decimal key pressed');
         }
+
     }
 });
 
