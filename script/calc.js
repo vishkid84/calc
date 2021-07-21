e2ae830015ae9517e568e289e43b939e8c2d1b32
vishkid84 <vishalittr@gmail.com>
2021-07-21 19:37:49 +0000

    added custom attributes for operator keys

--- script/calc.js
+++ script/calc.js
@@ -1,21 +1,29 @@
+const calculator = document.querySelector('.calculator')
+
 $('.calc_keys').click(function(e) {
     if ($( e.target ).is(":button")) {
         const key = e.target;
         const action = key.dataset.action;
         const display = document.getElementById('calcDisplay');
         const displayedNum = display.textContent;
+        const keyContent = key.textContent;
+        const previousKeyType = calculator.dataset.previousKeyType;
         // if no dataset, it would be a number
         if(!action){
             console.log(key.textContent);
-            if(displayedNum == 0){
-                display.textContent = key.textContent;
+            if(displayedNum === '0' || previousKeyType === 'operator'){
+                display.textContent = keyContent;
             }else{
-                display.textContent = displayedNum + key.textContent;
+                display.textContent = displayedNum + keyContent;
             }
         }
         // if there is a dataset
         if(action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide'){
             console.log('calculation keys pressed');
+            // Add custom attribute
+            calculator.dataset.previousKeyType = 'operator';
+            calculator.dataset.firstValue = displayedNum;
+            calculator.dataset.operator = action;
         }
         if(action === 'delete'){
             console.log('delete key pressed');
@@ -25,9 +33,10 @@ $('.calc_keys').click(function(e) {
         }
         if(action === 'equal'){
             console.log('equal key pressed');
+            const secondValue = displayedNum
         }
         if(action === 'decimal'){
-            console.log('decimal key pressed');
+            display.textContent = displayedNum + '.';
         }
 
     }
